97faa960d55c0d1c45e84fa59111e3ff30e0bab2
powerAn <25736019+powerAn2020@users.noreply.github.com>
2026-03-05 22:25:41 +0800

    update

--- scripts/customize.sh
+++ scripts/customize.sh
@@ -76,7 +76,7 @@ ui_print "- Setting permissions"
 set_perm_recursive "$MODPATH"                          0 0 0755 0644
 set_perm_recursive "$MODPATH/system/usr/libexec/ssh-core" 0 0 0755 0755
 set_perm           "$MODPATH/opensshd.init"            0 0 0755
-
+set_perm           "$MODPATH/*.sh"            0 0 0755
 # Make injected scripts executable
 [ -d "$MODPATH/scripts" ] && set_perm_recursive "$MODPATH/scripts" 0 0 0755 0755
 [ -f "$MODPATH/action.sh" ] && set_perm "$MODPATH/action.sh" 0 0 0755
